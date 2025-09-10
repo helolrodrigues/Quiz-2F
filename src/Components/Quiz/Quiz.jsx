ddf8ef5b65f9bc54f48327e9ab90477d0590d1a8
helolrodrigues <heloisa.rodrigues19@etec.sp.gov.br>
2025-09-10 16:58:22 -0300

    primeiro envio

---
+++ src/Components/Quiz/Quiz.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import './Quiz.css'
+
+const Quiz = () =>{
+  return(
+    <div className='container'>
+        <h1>Quiz App</h1>
+        <hr />
+        <h2>1. Qual dispositivo é necessário para a conexão com a Internet?</h2>
+        <ul>
+            <li>Modem</li>
+            <li>Router</li>
+            <li>LAN Cable</li>
+            <li>Pen Drive</li>
+        </ul>
+        <button>Next</button>
+        <div className="index">1 of 5 questions</div>
+    </div>
+  )
+}
+
+export default Quiz
